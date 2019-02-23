87a896fab2c0a008418da84ce183e366c6d90d09
MichaelALee <47281348+MichaelALee@users.noreply.github.com>
2019-02-23 17:20:18 +0000

    Main method for connecting to our SQL server.
    Will only work from university hosts.

---
+++ Server.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+public class Server
+{
+
+   public static void main (String[] args)
+   {
+        Connection connection = null;
+
+        try
+        {
+            String userName = "csc8005_team02";
+            String password = "HogsGet(Text";
+            String url = "jdbc:mysql://homepages.cs.ncl.ac.uk/csc8005_team02";
+            Class.forName ("com.mysql.cj.jdbc.Driver").newInstance ();
+            connection = DriverManager.getConnection (url, userName, password);
+            System.out.println ("Database connection established");
+        }
+        catch (Exception e)
+        {
+            System.err.println ("Cannot connect to database server");
+            System.err.println (e.getMessage ());
+        }
+       
+         /* Here we put all our back end functionality.
+          */
+        
+        finally
+        {
+           if (connection != null)
+           {
+               try
+               {
+                   connection.close ();
+                   System.out.println ("Database connection terminated");
+               }
+               catch (Exception e) { /* ignore close errors */ }
+           }
+        }
+   }
+}
