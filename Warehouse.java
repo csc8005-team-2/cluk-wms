3c3166828855c78e5f475ed9d67706dce5649a88
MichaelALee <47281348+MichaelALee@users.noreply.github.com>
2019-03-14 16:27:16 +0000

    Update Warehouse.java
    Updated sendOrder method

--- Warehouse.java
+++ Warehouse.java
@@ -68,51 +68,113 @@ import java.sql.*;
     
     public void SendOrder(Connection connection, int orderId) throws SQLException
     {
-        Statement statement = null;
-        String query = "SELECT stockItem, quantity " +
-                       "FROM Contains " +            
-                       "WHERE orderId='"+orderId+"'";
-        try {
-            statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(query);
-            
-            while(rs.next()){
-            	int Quantity = rs.getInt("quantity");
-            	String StockItem = rs.getString("StockItem");
-            	Statement statement2 = null;
-            	String query2 = "UPDATE Inside " +
-                            	"SET quantity = quantity-"+Quantity+           
-                            	" WHERE stockItem='"+StockItem+"' AND warehouseAddress ='"+Address+"'";
-            	
-            	try {
-                    statement2 = connection.createStatement();
-                    statement2.executeUpdate(query2);
-                } catch (SQLException e ) {
-                    e.printStackTrace();
-                } finally {
-                    if (statement2 != null) {statement2.close();}
-                }                     
-            }
-  
-        } catch (SQLException e ) {
-            e.printStackTrace();
-        } finally {
-            if (statement != null) {statement.close();}
-       }   
-        
-       Statement statement3 = null;
-       String query3 =  "UPDATE StockOrders "+
-    		   			"SET orderStatus = 'Complete' "+
-    		   			"WHERE orderId='"+orderId+"'";
-       try {
-           statement3 = connection.createStatement();
-           statement3.executeUpdate(query3);
-       } catch (SQLException e ) {
-           e.printStackTrace();
-       } finally {
-           if (statement3 != null) {statement3.close();}
-       }                     
+    	
+    	boolean orderFulfilled = false;
+    	
+    	while (orderFulfilled == false){
+    		
+    		Statement statement9 = null;
+	    	String query9 = "SELECT orderStatus "+
+	    					"FROM StockOrders "+
+	    					"WHERE orderId ='"+orderId+"'";
+	    	
+	    	try{
+				statement9 = connection.createStatement();
+				ResultSet rs = statement9.executeQuery(query9);
+			
+					rs.next();
+					String orderStatus = rs.getString("orderStatus");
+					if(!orderStatus.equalsIgnoreCase("Pending")){
+						System.out.println("Order has already been fulfilled.");
+						orderFulfilled = true;
+					}
+					} catch (SQLException e ) {
+						e.printStackTrace();
+					} finally {
+						if (statement9 != null) {statement9.close();}
+    		
+    		
+    		
+    	
+				boolean stockAvaliable = true;
+				while (stockAvaliable == true && orderFulfilled == false){
+				Statement statement0 = null;
+				String query0 = "SELECT c.quantity, I.quantity "+
+    					"FROM Contains c, Inside I "+
+    					"WHERE c.orderId ='"+orderId+"' OR I.warehouseAddress ='"+Address+"'";
+    	
+    	
+    	
+				try{
+					statement0 = connection.createStatement();
+					ResultSet rs = statement0.executeQuery(query0);
+		
+					while(rs.next()){
+						int CQuantity = rs.getInt("c.quantity");
+						int IQuantity= rs.getInt("I.quantity");
+						if(CQuantity > IQuantity){
+							System.out.println("Order cannot be fulfilled. Warehouse stock too low.");
+							stockAvaliable = false;
+				}
+					}
 
-    }
+		} catch (SQLException e ) {
+			e.printStackTrace();
+		} finally {
+			if (statement0 != null) {statement0.close();}
+
+    	
+
+    			Statement statement = null;
+    			String query = "SELECT stockItem, quantity " +
+    							"FROM Contains " +            
+    							"WHERE orderId='"+orderId+"'";
+    			try {
+    				statement = connection.createStatement();
+    				ResultSet rs = statement.executeQuery(query);
+            
+    				while(rs.next()){
+    					int Quantity = rs.getInt("quantity");
+    					String StockItem = rs.getString("StockItem");
+    					Statement statement2 = null;
+    					String query2 = "UPDATE Inside " +
+                            				"SET quantity = quantity-"+Quantity+           
+                            				" WHERE stockItem='"+StockItem+"' AND warehouseAddress ='"+Address+"'";
+            	
+    					try {
+    						statement2 = connection.createStatement();
+    						statement2.executeUpdate(query2);
+    					} catch (SQLException e ) {
+    						e.printStackTrace();
+    					} finally {
+    						if (statement2 != null) {statement2.close();}
+    					}                     
+    				}
+  
+    			} catch (SQLException e ) {
+    				e.printStackTrace();
+    			} finally {
+    				if (statement != null) {statement.close();}
+    			}   
+        
+    			Statement statement3 = null;
+    			String query3 =  "UPDATE StockOrders "+
+    							"SET orderStatus = 'Out for delivery' "+
+    								"WHERE orderId='"+orderId+"'";
+    			try {
+    				statement3 = connection.createStatement();
+    				statement3.executeUpdate(query3);
+    			} catch (SQLException e ) {
+    				e.printStackTrace();
+    			} finally {
+    				if (statement3 != null) {statement3.close();}
+    			}                     
+
+    			}
  
+    			}
+			}
+ 
+    	}
+    }
 }
