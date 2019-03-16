efe6b1c9978785da4f56f7b9a2500f6884c224c2
Mateusz Grabara <mateusz@grabara.eu>
2019-03-16 21:20:01 +0000

    Added menu button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +23,8 @@ import { AppComponent } from './app.component';
     MatSidenavModule,
     MatToolbarModule,
     MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   providers: [],
