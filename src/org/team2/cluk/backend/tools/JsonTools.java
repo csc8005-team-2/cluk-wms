8838697240585420ead2e2e744f973076f5557ed
Mateusz Grabara <mateusz@grabara.eu>
2019-04-02 13:08:15 +0100

    Created separate class to streamline JSON processing across the application

---
+++ src/org/team2/cluk/backend/tools/JsonTools.java
@@ -0,0 +1,43 @@
+package org.team2.cluk.backend.tools;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+
+/**
+ * Simple class to provide other classes, in particular web resources, with possibility to parse String into JSON.
+ *
+ * @author Matt Grabara
+ * @version 02/04/2019
+ */
+public class JsonTools {
+    /**
+     * Converts String object (e.g. http request body) into JsonObject.
+     * @param inputJson stringified JSON
+     * @return  parsed JSON
+     */
+    public static JsonObject parseObject(String inputJson) {
+        // parsing incoming json using javax.json library from Java EE
+        JsonReader jsonReader = Json.createReader(new StringReader(inputJson));
+        JsonObject jsonObject = jsonReader.readObject();
+        jsonReader.close();
+
+        return jsonObject;
+    }
+
+    /**
+     * Converts String object (e.g. http request body) into JsonArray.
+     * @param inputJsonArray    stringified JSON array
+     * @return  parsed JSON array
+     */
+    public static JsonArray parseArray(String inputJsonArray) {
+        // parsing incoming json array using javax.json library from Java EE
+        JsonReader jsonReader = Json.createReader(new StringReader(inputJsonArray));
+        JsonArray jsonArray = jsonReader.readArray();
+        jsonReader.close();
+
+        return jsonArray;
+    }
+}
