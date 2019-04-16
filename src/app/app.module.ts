5417708dbf95b5f7f4ecb1863ab29c17746e9b05
Nykky Allen <nykky.allen@ncl.ac.uk>
2019-04-16 13:31:18 +0100

    Changes in routing and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MaterialModule } from './material.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewOrderComponent } from './components/new-order/new-order.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { TotalStockComponent } from './components/warehouse/total-stock/total-stock.component';
+import { AddStockComponent } from './components/warehouse/add-stock/add-stock.component';
+import { RestaurantOrdersComponent } from './components/warehouse/restaurant-orders/restaurant-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NewOrderComponent,
+    MainMenuComponent,
+    TotalStockComponent,
+    AddStockComponent,
+    RestaurantOrdersComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    MatGridListModule,
-    MatSidenavModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
