08d281da4beee47cba443227aabc9cbb8c2c33e9
Nykky Allen <nykky.allen@ncl.ac.uk>
2019-04-16 13:27:51 +0100

    Changes to DataSourceFromTable

---
+++ src/app/classes/table-data-source.spec.ts
@@ -0,0 +1,7 @@
+import { TableDataSource } from './table-data-source';
+
+describe('TableDataSource', () => {
+  it('should create an instance', () => {
+    expect(new TableDataSource()).toBeTruthy();
+  });
+});
