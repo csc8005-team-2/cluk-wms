abdce2e589bfd9f59b75783e65b9a8bf7860ce26
Nykky Allen <nykky.allen@ncl.ac.uk>
2019-04-16 15:47:39 +0100

    Created warehouse manager component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { TotalStockComponent } from './components/warehouse/total-stock/total-stock.component';
 import { AddStockComponent } from './components/warehouse/add-stock/add-stock.component';
 import { RestaurantOrdersComponent } from './components/warehouse/restaurant-orders/restaurant-orders.component';
+import { WarehouseManagerComponent } from './components/warehouse/warehouse-manager/warehouse-manager.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { RestaurantOrdersComponent } from './components/warehouse/restaurant-ord
     MainMenuComponent,
     TotalStockComponent,
     AddStockComponent,
-    RestaurantOrdersComponent
+    RestaurantOrdersComponent,
+    WarehouseManagerComponent
   ],
   imports: [
     BrowserModule,
