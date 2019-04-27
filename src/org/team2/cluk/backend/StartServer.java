a88e24346cc7e1fdd2263123966c7f08fbc2f82c
Mateusz Grabara <mateusz@grabara.eu>
2019-04-27 13:25:12 +0100

    Added loading server configuration from external JSON file

--- src/org/team2/cluk/backend/StartServer.java
+++ src/org/team2/cluk/backend/StartServer.java
@@ -7,37 +7,43 @@ import com.sun.net.httpserver.HttpsServer;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.team2.cluk.backend.tools.DbConnection;
+import org.team2.cluk.backend.tools.JsonTools;
 import org.team2.cluk.backend.tools.ServerLog;
 import org.team2.cluk.backend.webresources.*;
 
+import javax.json.JsonObject;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLParameters;
 import javax.net.ssl.TrustManagerFactory;
 import javax.ws.rs.core.UriBuilder;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.net.URI;
 import java.security.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
- * Class containing main class used to start the server
+ * Class containing main class used to start the server. Server configuration provided as JSON file using --config parameter.
+ * JSON format:
+ *      {"keystore": string (optional), "keystorePassword": string (optional),
+ *      "dbURI": string, "dbUsername": string, "dbPassword": string}
  * <p><b>TO DO:</b></p>
  * <p><ol><li>
- *     Import DB settings from file
- * </li><li>
  *     Create way to exit application gracefully
  * </li><li>
  *     Export log to a file
  * </li></ol></p>
  *
- * @version 31/03/2019
+ * @version 27/04/2019
  */
 public class StartServer {
-    public static void main (String[] args) {
-        boolean noSsl = false;
+    public static void main(String[] args) {
+        boolean useSsl = false;
 
         // initialise URI and port
         final String listeningUri;
@@ -52,26 +58,52 @@ public class StartServer {
             arguments.add(argument.toLowerCase());
         }
 
-        // determine if --noSsl argument given
-        if (arguments.contains("--nossl")) {
-            noSsl = true;
+        // check if config file provided
+        if (!arguments.contains("--config") || arguments.indexOf("--config") == arguments.size()-1) {
+            ServerLog.writeLog("No configuration file provided");
+            System.exit(0);
         }
 
-        // initialise key path
-        final String keyStorePath =
-                (!noSsl && arguments.contains("--keystore") && arguments.indexOf("--keystore") != arguments.size()-1)
-                        ? arguments.get(arguments.indexOf("--keystore")+1) : "";
+        // get config file path
+        final String configFilePath = arguments.get(arguments.indexOf("--config")+1);
 
-        // get keystore password
-        final String keyStorePassword =
-                (!noSsl && arguments.contains("--password") && arguments.indexOf("--password") != arguments.size()-1)
-                        ? arguments.get(arguments.indexOf("--password")+1) : "";
+        // load config file
+        final Scanner configFile;
+        final StringBuilder configBuilder = new StringBuilder();
+        try {
+            configFile = new Scanner(new FileReader(configFilePath));
+            // read configuration to a string
+            while (configFile.hasNext()) {
+                configBuilder.append(configFile.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            ServerLog.writeLog("Configuration file not found in path specified");
+            System.exit(0);
+        }
+
+        // parse configuration string as a JSON
+        JsonObject config = JsonTools.parseObject(configBuilder.toString());
+
+        if (config.containsKey("keystore"))
+            useSsl = true;
+
+        // set up database connection
+        if (config.containsKey("dbURI") && config.containsKey("dbUsername") && config.containsKey("dbPassword")) {
+            String userName = config.getString("dbUsername");
+            String password = config.getString("dbPassword");
+            String uri = config.getString("dbURI");
+            // connect to the database
+            DbConnection.connect(userName, password, uri);
+        } else {
+            ServerLog.writeLog("Configuration file contains incomplete database connection settings");
+            System.exit(0);
+        }
 
         // initialise server resources
-        ResourceConfig config = new ResourceConfig(CorsFilter.class, Warehouse.class, Restaurant.class, Authorisation.class);
+        ResourceConfig resourceConfig = new ResourceConfig(CorsFilter.class, Warehouse.class, Restaurant.class, Authorisation.class);
 
         // build base URI based on whether SSL connection desired
-        if (noSsl) {
+        if (!useSsl) {
             listeningUri = "http://localhost/"; // server will be accessible under this URI
             listeningPort = 80; // server will be listening on this port
         } else {
@@ -82,25 +114,28 @@ public class StartServer {
         final URI baseUri = UriBuilder.fromUri(listeningUri).port(listeningPort).build();
 
         // if no SSL desired, launch server
-        if (noSsl) {
-            HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, config);
+        if (!useSsl) {
+            HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, resourceConfig);
         } else {
 
             // HTTPS server settings
             try {
                 // load keystore file
-                InputStream jksStream = new FileInputStream(keyStorePath);
+                InputStream jksStream = new FileInputStream(config.getString("keystore"));
 
                 // create SSL context
                 SSLContext sslContext = SSLContext.getInstance("TLS");
 
+                String keystorePassword = (config.containsKey("keystorePassword"))
+                        ? config.getString("keystorePassword") : "";
+
                 // initialise the keystore
                 KeyStore keyStore = KeyStore.getInstance("JKS");
-                keyStore.load (jksStream, keyStorePassword.toCharArray());
+                keyStore.load (jksStream, keystorePassword.toCharArray());
 
                 // setup the key manager factory
                 KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-                keyManagerFactory.init(keyStore, keyStorePassword.toCharArray());
+                keyManagerFactory.init(keyStore, keystorePassword.toCharArray());
 
                 // setup the trust manager factory
                 TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
@@ -110,7 +145,7 @@ public class StartServer {
                 sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
 
                 // create instance of HttpServer with SSL context
-                HttpServer httpServer = JdkHttpServerFactory.createHttpServer(baseUri, config, sslContext, false);
+                HttpServer httpServer = JdkHttpServerFactory.createHttpServer(baseUri, resourceConfig, sslContext, false);
 
                 // downcasting HttpServer to HttpsServer
                 if (httpServer instanceof HttpsServer) {
@@ -136,12 +171,5 @@ public class StartServer {
             }
         }
         ServerLog.writeLog("Server running on " + listeningUri + " listening on port " + listeningPort);
-
-        // database connection settings
-        String userName = "csc8005_team02";
-        String password = "HogsGet(Text";
-        String url = "jdbc:mysql://homepages.cs.ncl.ac.uk/csc8005_team02";
-        // connect to the database
-        DbConnection.connect(userName, password, url);
     }
 }
