e297cd724c7bcf73673c92520bf4cf813d8659c8
elliewoood <47889364+elliewoood@users.noreply.github.com>
2019-05-11 12:35:15 +0100

    Update ServerLog.java

--- src/org/team2/cluk/backend/tools/ServerLog.java
+++ src/org/team2/cluk/backend/tools/ServerLog.java
@@ -3,16 +3,17 @@ package org.team2.cluk.backend.tools;
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- * Simple logging system for the backend. Static method used to ensure one log is running for one server instance.
+/*
+ * ServerLog Class which provides a simple logging system for the backend
+ * Static method used to ensure one log is running for one server instance
  *
- * @author Matt Grabara
  * @version 31/03/2019
  */
 public class ServerLog {
     private static ArrayList<String> log = new ArrayList<>();
+    
 /*
- * This method can use to add log for the system.This class can ensure logs are working for the system.
+ * Method to add a log for the system
  */
     public static void writeLog(String msg) {
         Date currentDateTime = new Date();
