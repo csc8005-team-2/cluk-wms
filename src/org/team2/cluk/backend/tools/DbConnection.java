613d564ea9fdb4dd2c54f395a1acb72d791d7402
elliewoood <47889364+elliewoood@users.noreply.github.com>
2019-05-11 12:09:10 +0100

    Update DbConnection.java

--- src/org/team2/cluk/backend/tools/DbConnection.java
+++ src/org/team2/cluk/backend/tools/DbConnection.java
@@ -4,18 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Class for handling SQL connection for the backend
+/*
+ * DbConnection Class for handling SQL connection for the backend
  *
  * @version 31/03/2019
  */
+
 public class DbConnection {
     private static Connection connection = null;
-/*
- * This method can help users to establish connection to the database.Users need to input username,password and url.They are all String type.
- * If every thing goes right the system will show "Database connection established".
- * If something goes wrong the system will show "JDBC driver not found" or "Database connection error" or "Unrecognised exception encountered".
- */
+    
+   /*
+    * Method to allow users to establish a connection to the database
+    * Users input a username,password and url, all of type string
+    * If successful, the system will show "Database connection established" and connect to the server
+    * If it fails, the system will show "JDBC driver not found" or "Database connection error" or "Unrecognised exception encountered"
+    */
+    
     public static void connect(String userName, String password, String url) {
         try {
             // importing MySQL driver as per MySQL website
@@ -32,6 +36,11 @@ public class DbConnection {
         }
     }
 
+   /*
+    * Method to allow users to disconnect from the database
+    * If it fails, the system will show "Exception occurred when disconnecting from the database"
+    */
+    
     public static void disconnect() {
         try {
             connection.close();
