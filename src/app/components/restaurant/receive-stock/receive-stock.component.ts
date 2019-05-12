ae08bdabc8c0285dcce1a71adefd407d40a046ed
Mateusz Grabara <mateusz@grabara.eu>
2019-05-12 18:15:26 +0100

    Improved Receive Stock functionality at restaurants

--- src/app/components/restaurant/receive-stock/receive-stock.component.ts
+++ src/app/components/restaurant/receive-stock/receive-stock.component.ts
@@ -1,21 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {SessionService} from '../../../services/session.service';
 import {Router} from '@angular/router';
+import {OrderEntry} from '../../../classes/order-entry';
+import {MatDialog, MatTableDataSource} from '@angular/material';
+import {ViewReceivedOrderComponent} from '../view-received-order/view-received-order.component';
 @Component({
   selector: 'app-receive-stock',
   templateUrl: './receive-stock.component.html',
   styleUrls: ['./receive-stock.component.css']
 })
 export class ReceiveStockComponent implements OnInit {
-  
-  constructor(private session: SessionService, private router: Router) { }
+  pendingOrdersSub: any;
+  private pendingOrders: MatTableDataSource<OrderEntry>;
 
-  confirmReceipt(orderId: number) {
-    this.session.receiveOrder(this.session.getVenueAddress(), orderId).subscribe(res => {
-      window.alert('Order ' + orderId + ' successfully received. Stock amounts updated in the database.');
-      this.router.navigate(['']);
+  // displayed columns format
+  displayedColumns: string[] = ['orderId', 'dateTime', 'address', 'viewOrder'];
+
+  constructor(private session: SessionService, private router: Router, private dialog: MatDialog, private cdRef: ChangeDetectorRef) {
+    this.pendingOrdersSub = this.session.getTodaysOrders().subscribe(res => {
+      this.pendingOrders = new MatTableDataSource(res);
     }, err => {
       console.log(err);
+    });
+  }
+
+  viewOrder(order: OrderEntry[]) {
+    // this.session.setOrderView(orderContents);
+    const dialogRef = this.dialog.open(ViewReceivedOrderComponent, {
+      width: '600px',
+      data: order
+    });
+
+    dialogRef.afterClosed().subscribe(orderAccepted => {
+      if (orderAccepted) {
+        this.pendingOrdersSub.unsubscribe();
+        this.pendingOrdersSub = this.session.getPendingOrders().subscribe(res => {
+          this.pendingOrders = new MatTableDataSource(res);
+          this.cdRef.detectChanges();
+        }, err => {
+          console.log(err);
+        });
+      }
     });
   }
 
